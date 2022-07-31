ac87c732c1049000ce7b5f314499ed4d7aa1f92f
phoenix-force <paradox.joe.007@gmail.com>
2022-07-31 23:36:14 +0530

    indent problem fixed

--- genCore/settings.py
+++ genCore/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-     'default': {  
+    'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'genx',  
         'USER': 'root',  
